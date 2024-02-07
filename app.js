f859663521d093040deff27cb603834f87becefc
Bdkelly <benjaminkelly97@gmail.com>
2024-02-06 19:47:33 -0500

    First fix

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const { exec } = require('child_process');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.post('/api/search', (req, res) => {
     const { departureCity, arrivalCity, departureDate, returnDate } = req.body;
