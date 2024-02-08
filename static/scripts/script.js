ace9cd980cfa2f055f6c82c24d000350a7592fa3
Bdkelly <50244981+Bdkelly@users.noreply.github.com>
2024-02-08 15:30:12 -0500

    script

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,17 +2,26 @@ function runpython() {
     const departureCity = document.getElementById('departureCity').value;
     const arrivalCity = document.getElementById('arrivalCity').value;
     // Arguments to pass to the Python script
-    var arguments = { departureCity };
+    var arguments = { departureCity, arrivalCity };
 
         var xhr = new XMLHttpRequest();
         xhr.open('POST', '/run_python_script', true);
         xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                var responseData = JSON.parse(xhr.responseText);
-                console.log(responseData);
+        xhr.onload = function () {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                // Request was successful
+                console.log(JSON.parse(xhr.responseText));
+            } else {
+                // Request failed
+                console.error('Request failed with status:', xhr.status);
             }
         };
-        xhr.send(JSON.stringify({ arguments: arguments }));
-    }
     
+        xhr.onerror = function () {
+            // Error during the request
+            console.error('Request failed');
+        };
+    
+        xhr.send(JSON.stringify(requestData));
+    }
+        
