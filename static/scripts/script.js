a59f0b81688b856e5ca10dd267c7cc9b60fe2502
Bdkelly <50244981+Bdkelly@users.noreply.github.com>
2024-02-08 15:20:25 -0500

    script

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -3,20 +3,16 @@ function runPythonScript() {
     const arrivalCity = document.getElementById('arrivalCity').value;
     // Arguments to pass to the Python script
     var arguments = { departureCity };
-
-    // Make an AJAX request to run the Python script
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', '/run_python_script', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var responseData = JSON.parse(xhr.responseText);
-            console.log(responseData);
-        }
-        else {
-            console.error('Error running Python script:', xhr.statusText);
-          }
-    } 
-    };
-    xhr.send(JSON.stringify({ arguments }));
-}
+    
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/run_python_script', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var responseData = JSON.parse(xhr.responseText);
+                console.log(responseData);
+            }
+        };
+        xhr.send(JSON.stringify({ arguments: arguments }));
+    }
+    
