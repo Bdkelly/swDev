625a8ee3b285116613fe72ee84f5e6303c1f678b
Bdkelly <50244981+Bdkelly@users.noreply.github.com>
2024-02-08 14:01:31 -0500

    server fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { spawn } = require('child_process');
 
 const app = express();
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
 
 // Serve static files
 app.use(express.static('static'));
