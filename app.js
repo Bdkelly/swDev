748390e9388bacd4cc22422caffa159207c1184e
Bdkelly <50244981+Bdkelly@users.noreply.github.com>
2024-02-08 15:46:03 -0500

    first

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const bodyParser = require('body-parser');
 const { spawn } = require('child_process');
 const app = express();
 
-const port = process.env.PORT || 3000;
-
 // Serve static files
 app.use(express.static('static'));
 
@@ -14,13 +12,14 @@ app.use(bodyParser.json());
 // Define route to run Python script
 app.post('/run_python_script', (req, res) => {
     const arguments = req.body.arguments || [];
+    const depcity = arguments[0];
 
     // Execute the Python script with provided arguments
-    const pythonProcess = spawn('python', ['flask_app/other.py', ...arguments]);
+    const pythonProcess = spawn('python', ['flask_app/other.py', depcity]);
     
     pythonProcess.stdout.on('data', (data) => {
         const output = JSON.parse(data.toString());
-        res.json({ output });
+        console.log(output);
     });
 });
 
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,26 +2,26 @@ function runpython() {
     const departureCity = document.getElementById('departureCity').value;
     const arrivalCity = document.getElementById('arrivalCity').value;
     // Arguments to pass to the Python script
-    var requestData = { departureCity, arrivalCity };
+    var requestData = { departureCity };
 
         var xhr = new XMLHttpRequest();
         xhr.open('POST', '/run_python_script', true);
         xhr.setRequestHeader('Content-Type', 'application/json');
-        xhr.onload = function () {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                // Request was successful
-                console.log(JSON.parse(xhr.responseText));
-            } else {
-                // Request failed
-                console.error('Request failed with status:', xhr.status);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    // Request was successful
+                    console.log(JSON.parse(xhr.responseText));
+                } else {
+                    // Request failed
+                    console.error('Request failed with status:', xhr.status);
+                }
             }
         };
-    
         xhr.onerror = function () {
             // Error during the request
             console.error('Request failed');
         };
-    
         xhr.send(JSON.stringify(requestData));
     }
         
