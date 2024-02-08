e49abc1cb046548cf4b7e0d005f40546e8ea9f0e
Bdkelly <50244981+Bdkelly@users.noreply.github.com>
2024-02-08 09:09:38 -0500

    Edited for Flask

--- app.js
+++ app.js
@@ -9,20 +9,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.use(bodyParser.json());
-
-app.post('/runpython', (req, res) => {
-    const arguments = req.body.arguments || [];
-
-    // Execute the Python script with provided arguments
-    const pythonProcess = spawn('python', ['flask_app/python_script.py', ...arguments]);
-    
-    pythonProcess.stdout.on('data', (data) => {
-        const output = JSON.parse(data.toString());
-        res.json({ output });
-    });
+app.get('/home', function(req, res) {
+    request('http://127.0.0.1:5000/flask', function (error, response, body) {
+        console.error('error:', error); // Print the error
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        console.log('body:', body); // Print the data received
+        res.send(body); //Display the response on the website
+      });      
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on port:${port}`);
 });
