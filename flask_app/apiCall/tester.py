d8817d7f9020a2f351f1a7461ec1140787933e1a
Bdkelly <50244981+Bdkelly@users.noreply.github.com>
2024-02-08 10:17:39 -0500

    testerAPI

--- flask_app/apiCall/tester.py
+++ flask_app/apiCall/tester.py
@@ -1,5 +1,21 @@
 import sys
 import json
+##########################
+def main(depcity,arrcity):
+    url = "https://sky-scrapper.p.rapidapi.com/api/v1"
+    key = "d5a66d03bamsh50759c21ff62689p1884f5jsn80ade6452abe"
+    host = "sky-scrapper.p.rapidapi.com"
+    caller = AppCall(url,key,host)
+    return(flightget(caller,depcity,arrcity))
+
+def getapid(caller,location):
+    out = caller.getaps(location)
+    return out['data'][0]
+
+def flightget(caller,fromap = "LOND",toap = "new"):
+    fromapid = getapid(caller,fromap)
+    #toapid = getapid(caller,toap)
+    return json.dumps(fromapid)
 
 def process_arguments(args):
     # Your processing logic here
@@ -8,6 +24,8 @@ def process_arguments(args):
 
 if __name__ == "__main__":
     # Parse arguments
-    args = sys.argv[1:]
+    arrcity = sys.argv[2]
+    depcity = sys.argv[1]
+    print(main(depcity,arrcity))
     # Process arguments and print JSON data
-    print(process_arguments(args))
+    
