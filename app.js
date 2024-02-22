87408121d5398abd1144b7ff8c20c5d10ca5f556
Bdkelly <50244981+Bdkelly@users.noreply.github.com>
2024-02-22 10:17:32 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 //
 app.post('/Alive',(req,res) => {
-    const data  = req.body;
+    const data  = req;
     const pythonProcess = spawn('python', ['flask_app/useap.py']);
     pythonProcess.stdout.on('data',(data) => {
         const parsedData = JSON.parse(data.toString());
