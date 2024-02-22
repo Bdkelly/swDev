1e0b39cb13f0cd9b6ac8718e02e61ee030be0aa2
Bdkelly <50244981+Bdkelly@users.noreply.github.com>
2024-02-22 10:01:03 -0500

    Update useap.py

--- flask_app/useap.py
+++ flask_app/useap.py
@@ -1,5 +1,4 @@
 from AppCall import AppCall
-import sys
 import json
 ###########################
 ###
@@ -8,27 +7,11 @@ def main(city):
     key = "d5a66d03bamsh50759c21ff62689p1884f5jsn80ade6452abe"
     host = "sky-scrapper.p.rapidapi.com"
     caller = AppCall(url,key,host)
-    resp = flightget(caller,city)
-    jsoner = json.dumps(resp)
-    print(jsoner)
-###
-def getapid(caller,location):
-    out = caller.getaps(location)
-    return out['data'][0]
-###
-def flightget(caller,city):
-    cityid = getapid(caller,city)
-    return(cityid)
+    return json.dumps(check(caller))
 ###
 def check(caller):
     status = caller.seviceCheck()
-    print(status)
+    return status
 ##########################
 if __name__ == "__main__":
-    city = sys.argv[1]
-    '''
-    arrcity = sys.argv[1]    
-    depdate = sys.argv[2]
-    retdate = sys.argv[3]
-    '''
-    main(city)
+    print(main())
