233cac3c96f2fadbfd7f24e741bfbe63ac74d4d4
Bdkelly <50244981+Bdkelly@users.noreply.github.com>
2024-02-22 11:08:10 -0500

    Update useap.py

--- flask_app/useap.py
+++ flask_app/useap.py
@@ -3,11 +3,15 @@ import json
 ###########################
 ###
 def main(city):
+    '''
     url = "https://sky-scrapper.p.rapidapi.com/api/v1"
     key = "d5a66d03bamsh50759c21ff62689p1884f5jsn80ade6452abe"
     host = "sky-scrapper.p.rapidapi.com"
     caller = AppCall(url,key,host)
     return json.dumps(check(caller))
+    '''
+    vals = {"NAME":"BEMBELLY"}
+    return json.dumps(vals)
 ###
 def check(caller):
     status = caller.seviceCheck()
